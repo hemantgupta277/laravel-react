7a0446d360025298a1d4ea5c489751eabdb3d56b
TheHemantGupta <hemantgupta277@gmail.com>
2024-12-14 17:10:57 +0530

    First Commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+
+    public function index()
+    {
+        return inertia('Project/Index',[
+
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreProjectRequest $request)
+    {
+        //
+    }
+
+    public function show(Project $project)
+    {
+        //
+    }
+
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        //
+    }
+
+    public function destroy(Project $project)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_10_26_193732_create_projects_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->string('status');
+            $table->string('image_path')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
