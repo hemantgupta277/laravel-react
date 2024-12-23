6303595c4c1f79a45921e4e60284c26ecf7fdded
TheHemantGupta <hemantgupta277@gmail.com>
2024-12-23 23:33:52 +0530

    working on project create page

--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -19,7 +19,9 @@ export default function Create({auth}){
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            <form className="p-4 sm:p-8 bg-white shadow sm:rounded-lg" action="">
+                            <form
+                                onSubmit={onsubmit}
+                                className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                                 form
                             </form>
                         </div>
