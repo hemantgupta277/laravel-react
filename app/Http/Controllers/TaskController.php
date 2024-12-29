7a4d0755a545d5f6b5eb252d13ca6fa904aff9e6
TheHemantGupta <hemantgupta277@gmail.com>
2024-12-29 23:52:28 +0530

    working on task crud

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\ProjectResource;
 use App\Http\Resources\TaskResource;
+use App\Http\Resources\UserResource;
+use App\Models\Project;
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Response;
 use Inertia\ResponseFactory;
 
@@ -29,14 +35,27 @@ class TaskController extends Controller
         ]);
     }
 
-    public function create()
+    public function create(): Response|ResponseFactory
     {
-        //
+        $projects = Project::all();
+        $users = User::all();
+        return inertia('Task/Create', [
+            'projects' => ProjectResource::collection($projects),
+            'users' => UserResource::collection($users),
+        ]);
     }
 
     public function store(StoreTaskRequest $request)
     {
-        //
+        $data = $request->validated();
+        $image = $data['image']??null;
+        $data['created_by'] = Auth::id();
+        $data['updated_by'] = Auth::id();
+        if($image) {
+            $data['image_path'] = $image->store('tasks', 'public');
+        }
+        Task::create($data);
+        return to_route('tasks.index')->with('success', 'Task created successfully.');
     }
 
     public function show(Task $task)
@@ -51,11 +70,20 @@ class TaskController extends Controller
 
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $data = $request->validated();
+        $image = $data['image']??null;
+        $data['updated_by'] = Auth::id();
+        if($image) {
+            if($task->image) Storage::disk('public')->delete($task->image_path);
+        }
+        $data['image_path'] = $image->store('tasks', 'public');
+        $task->update($data);
+        return to_route('tasks.index')->with('success', 'Task updated successfully.');
     }
 
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return to_route('task.index');
     }
 }
--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head} from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import TasksTable from "@/Pages/Task/TasksTable.jsx";
 
 export default function Index({auth, tasks, queryParams = null}){
@@ -8,12 +8,20 @@ export default function Index({auth, tasks, queryParams = null}){
         <AuthenticatedLayout
             user={auth.user}
             header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Tasks
-                </h2>
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                        Tasks
+                    </h2>
+                    <Link
+                        href={route('task.create')}
+                        className="text-white bg-pink-700 px-3 py-1.5 rounded-lg font-bold hover:bg-pink-800"
+                    >
+                        Add New
+                    </Link>
+                </div>
             }
         >
-            <Head title="Tasks" />
+            <Head title="Tasks"/>
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
